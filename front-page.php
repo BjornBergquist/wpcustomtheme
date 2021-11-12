524407d66b9da22fb4e013b317be0ec0cba99d2c
Björn Bergquist <bjorn.bergquist@codic.se>
2021-11-12 11:25:39 +0100

    Did a lecture

--- front-page.php
+++ front-page.php
@@ -1,6 +1,23 @@
 <?php
-get_header()?>
-<h1 class="pageTitle">Ur vägen här kommer front-page.php!</h1>
-<?php get_template_part('/includes/section','content');?>
+get_header();
+$title = get_field('tile');
+$description = get_field('description');
+$image = get_field('pageimage');
+?>
+<?php if($title) : ?>
+    <h1 class="pageTitle"><?php echo $title; ?></h1>
+<?php else : ?>
+    <h1 class="pageTitle">Default Title</h1>
 <?php
+endif;
+if ($description): ?>
+    <p><?php echo str_replace("\n\r\n", "</p><p>",$description); ?></p> 
+<?php
+endif; 
+// \n mot <br />
+
+if ($image): ?>
+    <img class="pageimage" src="<?php echo $image['sizes']['medium'] ?>" alt="<?php echo $image['alt'] ?>">
+<?php
+endif; 
 get_footer();
